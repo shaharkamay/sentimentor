8f29b167d7a68b314c0cb9d7b242672fe6f2470a
shaharkamay <shahar.kamay@gmail.com>
2021-09-20 04:18:51 +0300

    done the result Section, and the color reflector

--- index.js
+++ index.js
@@ -15,10 +15,8 @@ async function fetchSentiment(text) {
         body,
     });
     const data = await response.json();
-
+    console.log(data);
     return data;
-
-
 }
 
 ( async () => {
@@ -29,6 +27,14 @@ async function fetchSentiment(text) {
 
 async function handleSendSentimEvent(e) {
     const text = e.target.previousElementSibling.value;
-    const sentiment = await fetchSentiment(text);
-    console.log(sentiment);
+    const data = await fetchSentiment(text);
+
+    const sentiment = data.result.type;
+
+    const resultsSection = document.querySelector("[id='results']");
+    resultsSection.classList = sentiment; // the class name is similar to the sentiment type
+
+    const [, sentimentType, polarity] = resultsSection.children;
+    sentimentType.textContent = `Sentiment: ${sentiment}`;
+    polarity.textContent = `Polarity: ${data.result.polarity}`;
 }
